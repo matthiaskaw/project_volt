c4d19cf4dc69e13f5473883490aa929eb152a672
Matthias <Matthias.Kawalek@t-online.de>
2024-07-02 20:13:47 +0200

    Worked on DataController

--- Controller/DataController.cs
+++ Controller/DataController.cs
@@ -1,5 +1,28 @@
+using System.IO;
+using Model.DatabaseModel;
 
 public class DataController{
+
+    private static DataController _instance;
+
+    private DataController(){
+
+
+    }
+
+    public static DataController Instance {
+        get{
+
+            if(_instance == null){
+                _instance = new DataController();
+            }
+            return _instance;
+        }
+    }
+
+    public string StorageDirectory{get; set;}
+    private ICRUD _database;   
+
 
 
     
---
+++ Model/ICRUD.cs
@@ -0,0 +1,12 @@
+namespace Model.DatabaseModel{
+
+    public interface ICRUD{
+
+
+        public void Create();
+        public void Read();
+        public void Update();
+        public void Delete();
+        
+    }
+}
